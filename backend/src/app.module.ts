d3e840c4664493ef17dbeb829ff99a0e535a4856
Izuchukwu Samson <izuchukwusamson5@gmail.com>
2023-06-25 13:15:47 +0100

    chore(modules): :hammer: Added the api modules
    modules

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TourModule } from './tour/tour.module';
+import { TourModule } from './modules/tour/tour.module';
+import { UserModule } from './modules/user/user.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { ReviewModule } from './modules/review/review.module';
 
 @Module({
-  imports: [],
+  imports: [TourModule, UserModule, AuthModule, ReviewModule],
   controllers: [AppController],
   providers: [AppService],
 })
