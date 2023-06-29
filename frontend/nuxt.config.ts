a6a9ce5a80349efe0c06cdca379c72ff7bc24da2
Izuchukwu Samson <izuchukwusamson5@gmail.com>
2023-06-29 06:17:59 +0100

    chore: :art: File updates

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -2,5 +2,25 @@
 
 export default defineNuxtConfig({
   // devtools: { enabled: true },
+  app: {
+    head: {
+      title: "Natours | Explore The World",
+      meta: [
+        { name: "description", content: "Discover What Nature has to offer" },
+        // <link />
+      ],
+      link: [
+        {
+          rel: "shortcut icon",
+          type: "image/png",
+          href: "@/assets/img/favicon.png",
+        },
+      ],
+      // script: [{ src: "../assets/js/script.js" }],
+    },
+    // pageTransition: { name: "page", mode: "out-in" },
+  },
   css: ["@/assets/css/style.css"],
+
+  modules: ["@pinia/nuxt"],
 });
