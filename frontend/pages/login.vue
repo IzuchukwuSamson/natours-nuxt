b61a93e44432b98f1e372751ab17b30f92245795
Izuchukwu Samson <izuchukwusamson5@gmail.com>
2023-06-30 10:35:20 +0100

    feat: :art: Code update
    Some updates on the codes

--- frontend/pages/login.vue
+++ frontend/pages/login.vue
@@ -11,4 +11,27 @@ main.main
                 input#password.form__input(type='password', placeholder='••••••••', required, minlength='8')
             .form__group
                 button.btn.btn--green Login
+        div(class="or") ------------- OR ---------------
+        div(class="btn-google btn btn-lg")
+            img(src="https://img.icons8.com/color/16/000000/google-logo.png") 
+            a(class="gLink" href="#") Log In With Google
 </template>
+
+<style>
+.btn-google {
+  color: #545454;
+  background-color: #ffffff;
+  box-shadow: 0 1px 2px 1px #ddd;
+  padding: 10px;
+  margin-top: 10px;
+}
+.gLink {
+  text-decoration: none;
+  color: gray;
+  padding: 1px;
+}
+
+.or {
+  padding: 12px;
+}
+</style>
